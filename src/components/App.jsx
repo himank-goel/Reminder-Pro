1dc29f0d29e882d3f5d45c06373b60ed73cc24a3
Himank Goel <himankgoel03@gmail.com>
2017-12-14 14:07:57 +0530

    Added setup for redux

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+// function() {
+//     return {
+//         type: 'ADD_REMINDER',
+//         payload: 
+//     }
+// }
+
+class App extends Component {
+    render() {
+        return (
+            <div className = "App">
+                <div className = "title">
+                Reminder Pro
+                </div>
+                <div className = "form-inline">
+                    <div className = "form-group">
+                        <input  
+                            className = "form-control"
+                            placeholder = "I have to ..."
+                        /> 
+                    </div>
+                    <button 
+                        type = "button"
+                        className = "btn btn-success"
+                    >
+                        Add Reminder
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
